63a70c2baeccf1127721da35d6f01ebcee46484d
Andreas Piening <andreas@silverstripe.com>
2010-03-10 23:57:41 +0000

    MINOR: cosmetic changes to the readme and changes for easy install

--- _config.php
+++ _config.php
@@ -5,13 +5,11 @@ if(defined('SS_DATABASE_CLASS')) $databaseConfig['type'] = SS_DATABASE_CLASS;
 
 if(array_search($databaseConfig['type'], array('SQLiteDatabase', 'SQLite3Database', 'SQLitePDODatabase')) !== false) {
 
-	$databaseConfig = array(
-		'type' => $databaseConfig['type'],
-		'database' => (defined('SS_DATABASE_PREFIX') ? SS_DATABASE_PREFIX : '') . $database . (defined('SS_DATABASE_SUFFIX') ? SS_DATABASE_SUFFIX : ''),
-		'path' => defined('SS_SQLITE_DATABASE_PATH') && SS_SQLITE_DATABASE_PATH ? SS_SQLITE_DATABASE_PATH : ASSETS_PATH . '/.sqlitedb/',   // where to put the database file
-		'memory' => true,    // run tests in memory
-	);
-	
+	if(empty($databaseConfig['path'])) $databaseConfig['path'] = defined('SS_SQLITE_DATABASE_PATH') && SS_SQLITE_DATABASE_PATH ? SS_SQLITE_DATABASE_PATH : ASSETS_PATH . '/.sqlitedb/';   // where to put the database file
+	$databaseConfig['database'] = (defined('SS_DATABASE_PREFIX') ? SS_DATABASE_PREFIX : '') . $databaseConfig['database'] . (defined('SS_DATABASE_SUFFIX') ? SS_DATABASE_SUFFIX : '');
+	if(!isset($databaseConfig['memory'])) $databaseConfig['memory'] = true; // run tests in memory
+	if(empty($databaseConfig['key'])) $databaseConfig['key'] = defined('SS_SQLITE_DATABASE_KEY')  && SS_SQLITE_DATABASE_KEY  ? SS_SQLITE_DATABASE_KEY :  'SQLite3DatabaseKey';
+
 	/**
 	 * set pragma values on the connection.
 	 * @see http://www.sqlite.org/pragma.html
@@ -26,7 +24,6 @@ if(array_search($databaseConfig['type'], array('SQLiteDatabase', 'SQLite3Databas
 		$databaseConfig['type'] = 'SQLitePDODatabase';
 	} else {
 		$databaseConfig['type'] = 'SQLite3Database';
-		$databaseConfig['key']  = defined('SS_SQLITE_DATABASE_KEY')  && SS_SQLITE_DATABASE_KEY  ? SS_SQLITE_DATABASE_KEY :  'SQLite3DatabaseKey';
 	}
 }
 
